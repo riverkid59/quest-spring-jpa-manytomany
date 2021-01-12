2bb732e5a97d638b8b4fe4832cee8ea19953ff73
Josselin EVRARD <josselin.evrard@sncf.fr>
2021-01-12 20:25:33 +0100

    manque que la liste des cours

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,13 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Course {
@@ -12,8 +16,19 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
 
     public Course() {
+    }
+    
+    public List<Wizard> getWizards() {
+    	return wizards;
+    }
+    
+    public void setWizards(List<Wizard> wizards) {
+    	this.wizards = wizards;
     }
 
     public Course(String name) {
